9f416a6826f363b214884635021f906c797bd256
Arnaud Landry <Arnaud Landry@scud.ezequiel25.com>
2017-08-26 17:38:55 -0400

    fix missing Download src + disable remove doanloadfolder

--- Setup_Workflow/Install.ps1
+++ Setup_Workflow/Install.ps1
@@ -25,8 +25,8 @@ workflow Noah {
     Param(
         $DownloadFolder = "C:\Packages\" #replace in InlineScipt if modified !
        )
-    Remove-Item $DownloadFolder -Force -Recurse
-    New-Item -Type Directory $DownloadFolder 
+    #Remove-Item $DownloadFolder -Force -Recurse
+    #New-Item -Type Directory $DownloadFolder 
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/xFunctions.ps1" -OutFile "$DownloadFolder\xFunctions.ps1"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/Download-Src.ps1" -OutFile "$DownloadFolder\Download-Src.ps1"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/Install-Modules.ps1" -OutFile "$DownloadFolder\Install-Modules.ps1"
