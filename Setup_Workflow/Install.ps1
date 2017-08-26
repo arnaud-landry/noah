851620a3128aa8a066b6c14264d84cd427bb7d1f
Arnaud Landry <Arnaud Landry@scud.ezequiel25.com>
2017-08-26 17:26:44 -0400

    test

--- Setup_Workflow/Install.ps1
+++ Setup_Workflow/Install.ps1
@@ -30,6 +30,8 @@ workflow Noah {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/xFunctions.ps1" -OutFile "$DownloadFolder\xFunctions.ps1"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/Download-Src.ps1" -OutFile "$DownloadFolder\Download-Src.ps1"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/Install-Modules.ps1" -OutFile "$DownloadFolder\Install-Modules.ps1"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arnaud-landry/noah/master/Setup_Workflow/Install-SQL.ps1" -OutFile "$DownloadFolder\Install-SQL.ps1"
+    
     InlineScript { C:\Packages\Download-Src.ps1 -xFunctionsPath $using:DownloadFolder\xFunctions.ps1 -DownloadFolder $using:DownloadFolder }
     InlineScript { C:\Packages\Install-Modules.ps1 -xFunctionsPath $using:DownloadFolder\xFunctions.ps1 }
 }
